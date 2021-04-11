dc54c5b00e59da76a09438aa4802bbcd26bee27a
Marck C. Guzman <marck@marck-devs.com>
2021-04-11 17:20:24 +0200

    Added Dir processing

--- PyMerge_Compiler.py
+++ PyMerge_Compiler.py
@@ -6,66 +6,69 @@ import sys
 import os
 
 # < CONSTANTS >
-__CMD_IMPORT__ = "import"
-__CMD_OUTFILE__ = "outFile"
-__CMD_COMPILE__ = "compile"
-__CMD_REMOVE_COMF__ = "rmCompile"
+__CMD_COMPILE__          = "compile"
+__CMD_IMPORT__           = "import"
+__CMD_OUTFILE__          = "outFile"
+__CMD_REMOVE_COMF__      = "rmCompile"
+__COMMAND_REGEX__        = r"((#)|(\/\/))\s*!(?P<cmd>\w*)\s\"?(?(1)((?P<file>.*)\"\s*)|(?P<args>.*))\n?$"
+__CMD_ARR__              = (__CMD_COMPILE__, __CMD_IMPORT__, __CMD_OUTFILE__,__CMD_REMOVE_COMF__)
+__COMMAND_WITH_STRING__  = (__CMD_IMPORT__, __CMD_COMPILE__, __CMD_OUTFILE__)
 __FILE_COMPILER_PREFIX__ = "_"
-__COMMAND_WITH_STRING__ = (__CMD_IMPORT__, __CMD_COMPILE__, __CMD_OUTFILE__)
-__CMD_ARR__ = (__CMD_COMPILE__, __CMD_IMPORT__, __CMD_OUTFILE__,__CMD_REMOVE_COMF__)
-__COMMAND_REGEX__ = r"\s*!(?P<cmd>\w*)\s\"?(?(1)((?P<file>.*)\"\s*)|(?P<args>.*))\n?"
-__OUT_FILE_NAME__ = "mergered.pymerge"
+__OUT_FILE_NAME__        = "mergered.pymerge"
 # </ CONSTANT >
 
-__out_file__      = None
-__args__          = None
-__cmd_array__     = []
-__file_arr__      = []
+# < VARS >
+__args__         = None
+__cmd_array__    = []
+__file_arr__     = []
+__files2delete__ = []
+__out_file__     = None
+# </ VARS >
 
 # <Terminal colors>
 class LinColor:
-    NOTICE  = '\033[94m'
-    SUCCESS = '\033[92m'
-    WARNING = '\033[93m'
-    ERROR   = '\033[91m'
-    RESET   = '\033[0m'
+    ERROR   = "\033[91m"
+    NOTICE  = "\033[94m"
+    RESET   = "\033[0m"
+    SUCCESS = "\033[92m"
+    WARNING = "\033[93m"
     
 class WinColor:
-    NOTICE  = "color 3"
-    SUCCESS = "color 2"
-    WARNING = 'color 6'
     ERROR   = "color 4"
+    NOTICE  = "color 3"
     RESET   = "color 0"
+    SUCCESS = "color 2"
+    WARNING = "color 6"
 
-def err(msg, end="\n"):
+def err(msg, end = "\n"):
     color = LinColor.ERROR
     if os.name == "nt":
         color = ""
         os.system(WinColor.ERROR)
     print("{x}{msg}".format(msg=msg, x=color), end=end)
     
-def success(msg, end="\n"):
+def success(msg, end = "\n"):
     color = LinColor.SUCCESS
     if os.name == "nt":
         color = ""
         os.system(WinColor.SUCCESS)
     print("{x}{msg}".format(msg=msg, x=color), end=end)
     
-def p(msg ="", end="\n"):
+def p(msg ="", end = "\n"):
     color = LinColor.RESET
     if os.name == "nt":
         color = ""
         os.system(WinColor.RESET)
     print("{x}{msg}".format(msg=msg, x=color), end=end)
     
-def warn(msg, end="\n"):
+def warn(msg, end = "\n"):
     color = LinColor.WARNING
     if os.name == "nt":
         color = ""
         os.system(WinColor.WARNING)
     print("{x}{msg}".format(msg=msg, x=color), end=end)
     
-def notice(msg, end="\n"):
+def notice(msg, end = "\n"):
     color = LinColor.NOTICE
     if os.name == "nt":
         color = ""
@@ -73,6 +76,7 @@ def notice(msg, end="\n"):
     print("{x}{msg}".format(msg=msg, x=color),  end=end)
 # </ Terminal colors >
 
+# File exists function
 def exists(file):
     if not os.path.exists(file):
         return False
@@ -80,22 +84,38 @@ def exists(file):
         __file_arr__.append(file)
         return True
 
-
+# Create the compile file name and add to the delete files list
 def generate_compile_file(file):
-    return "{}{}".format(__FILE_COMPILER_PREFIX__, file)
+    if file.rfind(os.path.sep) == -1:
+        return f"{__FILE_COMPILER_PREFIX__}{file}"
+    path_separator_index = file.rindex(os.path.sep)
+    f_name = file[path_separator_index+1:]
+    _dir = file[0:path_separator_index+1]
+    return f"{_dir}{__FILE_COMPILER_PREFIX__}{f_name}"
+
+# delete compiling files
+def del_files():
+    p()
+    notice("Removing compiling files")
+    for f in __files2delete__:
+        warn(f"\tRemoving {f}")
+        os.remove(f)
+    p()
             
-# Para la compilacion se lee el archivo linea a linea
-# se procesa cada linea y si hay alguna importacion
-# primero se compila el archivo y luego se importa el
-# archivo compilado
-def compile(file):
-    # generate the cimpile file name
-    # TODO: procces paht not only files in current dir
+# For the compile proces, it read line by line and processed it
+# to look for any keyword, the command of the compiler
+# for this job rewrite the file into another, the compile file,
+# when compile finish, the file can be used to be imported in other files.
+# If there is any error or compile sucessfully al compile files will be removed
+def compile(file, delete = True):
+    
     if not exists(file):
         err("File {} not exists!".format(file))
         exit(2)
     
     outF = generate_compile_file(file)
+    if not delete:
+        __files2delete__.append(outF)
     altName = None
     notice("Compiling {}".format(file) )
     p()
@@ -111,9 +131,9 @@ def compile(file):
                 is_process = False
                 for match in regex:
                     is_process = True
-                    # guardamos los grupos de las coinidencias
+                    # save group that match wiht the regex
                     groups = match.groupdict()
-                    # procesamos el commando:
+                    # get the command of the groups
                     cmd = groups["cmd"]
                     args = ""
                     # get the arguments for the command
@@ -122,34 +142,34 @@ def compile(file):
                         if args == "" or args == None:
                             err("{} - Line {} : File name not found in {} command".format(file,lineN,cmd))
                             p()
+                            if delete:
+                                __files2delete__.append(outF)
+                            else:
+                                return
+                            del_files()
                             exit(2)
                     else:
                         args = groups["args"]
                         if args == "" or args == None:
                             err("{} - Line {} : Arguments not found in {} command".format(file,lineN, cmd))
                             p()
+                            if delete:
+                                __files2delete__.append(outF)
+                            else:
+                                return
+                            del_files();
                             exit(2)
-                    # if cmd == __CMD_COMPILE__:
-                    #     notice("Start subprocess:", end="")
-                    #     warn("{} - Compiling {}".format(file,args))
-                    #     p()
-                    #     compile(args)
-                    #     continue
                     if cmd == __CMD_IMPORT__:
                         importFile = generate_compile_file(args)
                         notice("{}: Import {}".format(file, args))
                         if not exists(importFile):
-                            compile(args)
-                            # p()
-                            # err("{} - Line {} : File {} not fount! - ".format(file,lineN,importFile), end="")
-                            # p("You must compile file before import!")
-                            # p()
-                            # exit(1)
+                            # if not exists the compile file, compile the original
+                            # file an then import it
+                            compile(args, False)
                         with open(importFile) as inF:
                             for inLine in inF:
                                 f.write(inLine)
                         f.write("\n")
-                        os.remove(importFile)
                         continue
                     if cmd == __CMD_OUTFILE__:
                         altName = args
@@ -161,12 +181,9 @@ def compile(file):
                     f.write(l)
     if altName != None:
         os.rename(outF, altName)
+    if len(__files2delete__) > 0 and delete:
+        del_files()
            
-def concat(files, out_file = __OUT_FILE_NAME__):
-    if exists(out_file):
-        os.remove(out_file)
-    for f in files:
-        compile(f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compile Files")
@@ -178,6 +195,5 @@ if __name__ == "__main__":
                 compile(file)
             else:
                 err("File {} not exists!".format(file))
-    
-# compile("test.js")
+
 p()
--- README.md
+++ README.md
@@ -1,10 +1,72 @@
 # PyMerge
-Concatenate files into one.
 
-Pymerge is a development tools to concatenate files into one.
+PyMerge has two variants:
+- **PyMerge OptionFile** witch process the pymerge config file and concatenate the files as specified in the file.
+- **PyMerge Compiler** whitch compile the file that passed as argument and interpreted line by line of the file searching the specific command to execute.
+---
+## PyMerge OptionFile
+Read the `pymerge` file and concatenate the files.
+Example:
+```
+outFile "mi_concat_file_name"
+import "file1.txt"
+import "file2.txt"
+```
+- `outFile "<name>"` set the output file name
+- `import "<file>"` set to concatenate file to the output file
 
-To initialize it need a config file `pymerge` wich set the order of the concatenation and the output file.
+The tool will follow the documet order.
 
-To set an file to concatenate you must use the keyword `import` and the file path into doble quotes(`"`): `import "index.js"`.
+## PyMerge Compiler
+The compiler read the file that pass as argument and process it, line by line looking for keywords.
 
-To set the output file's name you can use `outFile` and the name between double quotes(`"`): `outFile "dist.js"`.
+This keywords can be a long the file in a line of comment and it has a specific format:
+`# !cmd <arg>`.
+
+Example:
+### File a.js
+```javascript
+function action(msg){
+    alert(msg);
+}
+```
+### File b
+```javascript
+\\ !outFile "HelloWorld.js"
+\\ !import "a.js"
+
+action("Hello World");
+```
+
+### HelloWorld.js
+```javascript
+function action(msg){
+    alert(msg);
+}
+
+action("Hello World");
+```
+
+! Only accept one line comments and the most commons comment chars: `//` and `#`.
+For insert a command in multiline comment or in other comments tags:
+
+This hasn't got sense:
+```
+
+/*
+    multiline comment
+    // !import "file.js"
+*/
+
+```
+This is more usefull for lenguage that only have multiline comments
+```
+/* # !import "file.sql" */
+
+for HTML
+<!-- # !import "header.html" -->
+```
+
+In the copiler the command are similar:
+- `[// | #] !import "<file name>"` - for compile an import the file
+- `[// | #] !outFile "<file_name>"` - to set the final name of the out file
